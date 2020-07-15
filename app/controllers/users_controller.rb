b69b8fc8ce5155fa0fd1a3a20248f9855f7a88ed
vagrant <vagrant@localhost.localdomain>
2020-07-15 12:41:27 +0000

    [Update]7/15 21:41

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
 	def index
+	@user = User.new
 	end
 
     def top
     end
 
 	def new
+	@user = User.new
 	end
 
 	def show
 	end
-end
+
+    def edit
+    @user = User.find(params[:id])
+    end
+
+    def update
+     @user = User.find(params[:id])
+     @user.update(user_params)
+     redirect_to user_path(@user.id)
+    end
+
+   private
+   def user_params
+  params.require(:user).permit(:name, :profile_image)
+   end
+   end
