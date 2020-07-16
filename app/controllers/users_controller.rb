a70ed406e9bab6de8fa82f086c2a9a15b4ff4703
vagrant <vagrant@localhost.localdomain>
2020-07-16 08:27:55 +0000

    7/16 17:27

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,32 @@
 class UsersController < ApplicationController
-	def index
-	@user = User.new
-	end
 
-    def top
-    end
+  def index
+    @user = User.new
+  end
 
-	def new
-	@user = User.new
-	end
+  def top
+  end
 
 	def show
 	end
 
-    def edit
+  def edit
     @user = User.find(params[:id])
-    end
+  end
 
-    def update
-     @user = User.find(params[:id])
-     @user.update(user_params)
-     redirect_to user_path(@user.id)
-    end
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+    redirect_to books_path
+  else
+    @users=Users.all
+    render 'index'
+  end
+  end
 
-   private
-   def user_params
-  params.require(:user).permit(:name, :profile_image)
-   end
-   end
+  private
+   
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'users#top'
+  
+  devise_for :users
+  
   resources:books
 
 
